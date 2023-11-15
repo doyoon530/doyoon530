71fe41ae86d6b8f3794b9c84daad5f54c7af70a5
doyoon530 <150874253+doyoon530@users.noreply.github.com>
2023-11-15 16:53:35 +0900

    Update update_blogPost.py

--- update_blogPost.py
+++ update_blogPost.py
@@ -15,8 +15,7 @@ for idx, entrie in enumerate(rss_feed['entries']):
   latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]({entrie['link']})\n"
 
 preREADME = """
-![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=doyoon530&show_icons=true&theme=radical)
-
+![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=doyoon530&show_icons=true&theme=radical)<br>
 [![Naver blog Badge](https://img.shields.io/badge/-Naver%20blog-brightgreen?style=flat-square&logo=Naver&logoColor=white&link=https://blog.naver.com/kimdu001)](https://blog.naver.com/kimdu001)
 """
 
