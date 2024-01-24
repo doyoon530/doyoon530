8044bddd1bfaa0d82fd4203e2d4787e2db045ba8
KimDoYoon <150874253+doyoon530@users.noreply.github.com>
2024-01-24 20:29:36 +0900

    Update update_blogPost.py

--- update_blogPost.py
+++ update_blogPost.py
@@ -15,9 +15,9 @@ for idx, entrie in enumerate(rss_feed['entries']):
   latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]({entrie['link']})\n"
 
 preREADME = """
-Dissertation
-자이로 센서 데이터를 활용한 양치 위치 추정 및 비지도 학습 클러스터링을 통한 검증
-https://www.dbpia.co.kr/journal/articleDetail?nodeId=NODE11646290
+논문 Dissertation<br>
+자이로 센서 데이터를 활용한 양치 위치 추정 및 비지도 학습 클러스터링을 통한 검증<br>
+https://www.dbpia.co.kr/journal/articleDetail?nodeId=NODE11646290<br><br>
 
 ![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=doyoon530&show_icons=true&theme=radical)<br>
 [![Naver blog Badge](https://img.shields.io/badge/-Naver%20blog-brightgreen?style=flat-square&logo=Naver&logoColor=white&link=https://blog.naver.com/kimdu001)](https://blog.naver.com/kimdu001)
