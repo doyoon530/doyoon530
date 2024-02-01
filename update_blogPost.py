c8d72c7965c2d1fe1a025d6fe12c1bf40c46a3d8
KimDoYoon <150874253+doyoon530@users.noreply.github.com>
2024-02-01 22:19:24 +0900

    Update update_blogPost.py

--- update_blogPost.py
+++ update_blogPost.py
@@ -20,6 +20,9 @@ preREADME = """
 ## 논문 Dissertation<br>
 [자이로 센서 데이터를 활용한 양치 위치 추정 및 비지도 학습 클러스터링을 통한 검증](https://www.dbpia.co.kr/journal/articleDetail?nodeId=NODE11646290)<br><br>
 
+## 대회 Competition<br>
+제 1회 ART-AWS 리빙랩글로컬 해커톤 🥈<br><br>
+
 ## 연결 Link<br>
 [![Naver blog Badge](https://img.shields.io/badge/-Naver%20blog-brightgreen?style=flat-square&logo=Naver&logoColor=white&link=https://blog.naver.com/kimdu001)](https://blog.naver.com/kimdu001)
 """
